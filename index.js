2da604ef6e4973c3aafc8833d45d1ce94f1a4ae3
Chris Hadley <hadleyc15@yahoo.com>
2020-07-05 17:46:22 -0600

    lots of updates cause im a dummy and forgot to commit my changes

--- index.js
+++ index.js
@@ -1,72 +1,192 @@
 const fs = require("fs");
-const inquire = require("inquirer");
-const manager = require("./lib/manager");
-const engineer = require("./lib/engineer");
-const intern = require("./lib/intern");
+const inquirer = require("inquirer");
+const generateHTML = require("./template");
+const Manager = require("./lib/employee");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Employee = require("./lib/employee");
 
-const questionList = [
-    {
-        name: "role",
-        message: "Enter your job role",
-        type: "list",
-        choices: ["Manager", "Engineer", "Intern"]
-    },
-    {
-        name: "name",
-        message: "Enter your name",
-        type: "input",
-    },
-    {
-        name: "empId",
-        message: "Enter your employee ID",
-        type: "input",
-    },
-    {
-        name: "email",
-        message: "Enter your Email",
-        type: "input",
-        validate: function (empEmail) {
 
-            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(empEmail)
+const employeeList = [];
 
-            if (valid) {
-                return true;
-            } else {
-                console.log("Please enter a valid email")
-                return false;
+
+function profileGen() {
+    console.log("Welcome to the Team Profile Generator. You will enter your manager first, followed by the rest of your team.");
+    createFile("team.html", generateHTML.generateHTML());
+    newManager();      
+}
+
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "employeeRole",
+                message: "What is the new employee's role?",
+                choices: ['Engineer', 'Intern']
             }
+        ])
+        .then(function (data) {
+            if (data.employeeRole === "Engineer") {
+                newEngineer();
+            }else{
+                newIntern();
+            }
+        });       
+}
+
+function newManager() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "title",
+                message: "Please verify the employee's title:",
+                choices: ["Manager"]
+            },
+            {
+                type: "input",
+                name: "name",
+                message: "What is the manager's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is the manager's ID number?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is the manager's email address?"
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "What is the manager's office number?"
+            }
+        ])
+        .then(function(data) {
+            let manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+            employeeList.push(manager);
+                addToFile("team.html", generateHTML.addManagerCard(data));
+                addAnother();
+        });
+}
+
+function newEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "title",
+                message: "Please verify your selection:",
+                choices: ["Engineer"]
+            },
+            {
+                type: "input",
+                name: "name",
+                message: "Please enter the new engineer's name."
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Please enter the new engineer's ID number."
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please enter the new engineer's ID email address."
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "Please enter the new engineer's GitHub username."
+            }
+        ])
+        .then(function(data) {
+            let engineer = new Engineer(data.name, data.id, data.email, data.github);
+            employeeList.push(engineer);
+            addToFile("team.html", generateHTML.addEngineerCard(data));
+            addAnother();
+        });
+}
+
+function newIntern() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "title",
+                message: "Please verify your selection:",
+                choices: ["Intern"]
+            },
+            {
+                type: "input",
+                name: "name",
+                message: "Please enter the new intern's name."
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Please enter the new intern's ID number."
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please enter the new intern's ID email address."
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "Please enter the new intern's school."
+            }
+        ])
+        .then(function(data) {
+            let intern = new Intern(data.name, data.id, data.title, data.email, data.school);
+            employeeList.push(intern);
+            addToFile("team.html", generateHTML.addInternCard(data));
+            addAnother();
+        });
+}
+
+function addAnother() {
+    inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "addAnother",
+            message: "Would you like to add another employee?",
+            choices: ['Yes', 'No']
         }
-    },
-    {
-        name: "empSchool",
-        message: "Enter your School",
-        type: "input",
-        when: function (answers) {
-            return answers.empRole === "Intern"
+    ])
+    .then(function(data) {
+        if(data.addAnother === "Yes") {
+            addEmployee();
+        }else{
+            finish();
         }
-    },
-    {
-        name: "empOffice",
-        message: "Enter your Office number",
-        type: "input",
-        when: function (answers) {
-            return answers.empRole === "Manager"
+    });
+} 
+
+function createFile(html, data) {
+    fs.writeFile(html, data, function(err) {
+        if (err) {
+            return Error;
         }
-    },
-    {
-        name: "empGitHub",
-        message: "Enter your GitHub username",
-        type: "input",
-        when: function (answers) {
-            return answers.empRole === "Engineer"
+    });
+}
+
+function addToFile (html, data) {
+    fs.appendFile(html, data, function(err) {
+        if (err) {
+            return Error;
         }
-    },
-]
+    });
+}
+
+function finish() {
+    console.log("Your team profile has been generated! Check out the team.html file.")
+};
 
 
-
-
-
-
-
-function writeHTMLFile()
+profileGen();
---
+++ template.js
@@ -0,0 +1,98 @@
+var generateHTML = {
+    generateHTML: function (data) {
+        return `<!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" 
+            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+            <title>Team Profile Generator</title>
+        </head>
+
+        <body>
+
+            <div class="container-fluid bg-danger text-white text-center py-4">
+                <h2>My Team</h2>
+            </div>
+
+            <div class="container">
+                <div class="row mt-2 justify-content-center" id="TeamCards">
+                
+                
+                
+                </div>
+            </div>
+
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+            crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+            crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossorigin="anonymous"></script>
+    </body>
+
+    </html>
+            `;
+    },
+    addManagerCard: function (data) {
+        return `<div class="container">
+        <div class="card m-2" style="width: 18rem;" id="TeamCards">
+            <div class="card-header bg-primary">
+                <h2 class="card-title">${data.name}</h2>
+                <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${data.title}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID#: ${data.id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${data.email}">${data.email}</a></li>
+                    <li class="list-group-item">Office#: ${data.officeNumber}</li>
+                </ul>
+            </div>
+         </div>`
+    },
+
+    addEngineerCard: function (data) {
+        return `<div class="container">
+        <div class="card m-2" style="width: 18rem;" id="TeamCards">
+            <div class="card-header bg-primary">
+                <h2 class="card-title">${data.name}</h2>
+                <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${data.title}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID#: ${data.id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${data.email}">${data.email}</a></li>
+                    <li class="list-group-item">Github: <a href="github.com/ ${data.github}">github.com/${data.github}</a></li>
+                </ul>
+            </div>
+         </div>`
+    },
+
+    addInternCard: function (data) {
+        return `<div class="container">
+        <div class="card m-2" style="width: 18rem;" id="TeamCards">
+            <div class="card-header bg-primary">
+                <h2 class="card-title">${data.name}</h2>
+                <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${data.title}</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">ID#: ${data.id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${data.email}">${data.email}</a></li>
+                    <li class="list-group-item">School: ${data.school}</li>
+                </ul>
+            </div>
+         </div>`
+    },
+
+}
+
+
+
+module.exports = generateHTML
